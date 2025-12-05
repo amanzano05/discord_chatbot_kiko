ac11f13ff6f8e33f12901887b68de20b3737a025
amanzano05 <amanzano05@icloud.com>
2025-12-04 23:30:55 -0600

    refactor: switch to Waitress for serving the Flask application.

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+from waitress import serve
 
 app = Flask('')
 
@@ -8,7 +9,8 @@ def home():
     return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  # Use waitress for production-ready server
+  serve(app, host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
